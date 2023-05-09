286d835f52fe7a8fa83a0b31d913ec96004c8eca
Congo Justus Migue <congomigue@gmail.com>
2023-05-09 16:21:28 +0000

    more fixes #! to #6

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+# print the first characters int the range
+print("{:c}".format(97), end="")
+
 # so to Loop through the ASCII values for lowercase letters
-for i in range(97, 123):
+for i in range(98, 123):
     # print the corresponding character, followed by no space or new line
     print("{:c}".format(i), end="")
