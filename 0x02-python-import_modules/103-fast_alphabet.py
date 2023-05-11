104ce1bf898cce9f7a69c199b233251a0572e336
Congo Justus Migue <congomigue@gmail.com>
2023-05-11 21:55:35 +0000

    made  103-fast_alphabet.py bug free

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-import sys
-sys.stdout.write(''.join(map(chr, range(65, 91))) + '\n')
+
+if __name__ == "__main__":
+    import sys
+    sys.stdout.write(''.join(map(chr, range(65, 91))) + '\n')
