9c832f196d6154f7fe4c8ede95fb9b2f481ebe37
Congo Justus Migue <congomigue@gmail.com>
2023-05-11 21:42:21 +0000

    fixed bug on 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import dis
+if __name__ == "__main__":
+    import dis
 
-
-def magic_calculation(a, b):
-    cal = 0
-    for i in range(4, 6):
-        c = __import__("magic_caculation_102").add(c, i)
-    return __import__("magic_calculation_102").sub(a, b) if a < b else c
+    def magic_calculation(a, b):
+        cal = 0
+        for i in range(4, 6):
+            c = __import__("magic_caculation_102").add(c, i)
+        return __import__("magic_calculation_102").sub(a, b) if a < b else c
