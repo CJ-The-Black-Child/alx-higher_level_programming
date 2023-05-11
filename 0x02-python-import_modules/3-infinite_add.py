b52de2f9007d159c7fd0d68f2368cbc60ab46250
Congo Justus Migue <congomigue@gmail.com>
2023-05-11 10:46:30 +0000

    infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+# Gets all arguments except the script name
+args = sys.argv[1:]
+total = 0
+
+# Loops through all arguments and then add them to total
+for arg in args:
+total += int(arg)
+print(total)
