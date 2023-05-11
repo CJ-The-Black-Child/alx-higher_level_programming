f7aa869c9135ed61615b84c52948acb25db90ec6
Congo Justus Migue <congomigue@gmail.com>
2023-05-11 21:13:12 +0000

    trial #0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 
+if __name__ == "__main__":
 
+    from add_0 import add
 
-
-from add_0 import add
-
-def main():
     a = 1
     b = 2
 
     result = add(a, b)
 
     print(f"{a} + {b} = {result}")
-
-if __name__ == "__main__":
-    main()
