a9e022ffc48591b7a34b8ad0531d3ad91ad36e7c
Congo Justus Migue <congomigue@gmail.com>
2023-05-11 21:34:10 +0000

    fixed pep8 styling error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     else:
         n = len(argv) - 1
         if n == 1:
-             print("1 argument:")
+            print("1 argument:")
         else:
-             print("{} arguments:".format(n))
+            print("{} arguments:".format(n))
         for i in range(1, len(argv)):
-             print("{}: {}".format(i, argv[i]))
+            print("{}: {}".format(i, argv[i]))
