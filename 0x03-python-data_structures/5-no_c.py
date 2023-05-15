ff4ea40e88a7ee719ee5730b716c3356e3a2c3b7
Congo Justus Migue <congomigue@gmail.com>
2023-05-15 16:02:32 +0000

    #5 fixes

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
+
 def no_c(my_string):
     new_string = ""
 
     for char in my_string:
         if char != 'c' and char != 'c':
-           new_string += char
+            new_string += char
     return new_string
