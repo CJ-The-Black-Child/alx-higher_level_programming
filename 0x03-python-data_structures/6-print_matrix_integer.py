6cc2a4a00e08e39e5d49cd9e7210c37f03545214
Congo Justus Migue <congomigue@gmail.com>
2023-05-16 09:33:37 +0000

    #6 Qa Review test 1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,5 @@ def print_matrix_integer(matrix=None):
         matrix = []
     for row in matrix:
         for i in range(len(row)):
-            if i == len(row) - 1:
-                print("{:d}".format(row[i]))
-            else:
-                print("{:d} ".format(row[i]), end="")
+            print("{:d}".format(row[i]), end=" ")
     print()
