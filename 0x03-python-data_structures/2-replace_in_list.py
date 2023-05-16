707389e9aaf042e29ed01146ec9227fc6778f07b
Congo Justus Migue <congomigue@gmail.com>
2023-05-16 09:06:00 +0000

    #2 QA Review test 4

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,10 +4,6 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    new_list = []
-    for i in range(len(my_list)):
-        if i == idx:
-            new_list.append(element)
-        else:
-            new_list.append(my_list[i])
+    new_list = my_list[:]
+    new_list[idx] = element
     return new_list
