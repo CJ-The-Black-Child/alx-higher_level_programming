4f9daa39e5b133e8252304f25001865abbc59f49
Congo Justus Migue <congomigue@gmail.com>
2023-05-17 21:47:37 +0000

    #16 fixes

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,16 +1,44 @@
 #include <Python.h>
 
-void print_python_list(PyObject *p) {
+/**
+ * print_python_list - Prints the basic info about Python Lists
+ * @p: PyObject pointer
+ */
+
+void print_python_list(PyObject *p)
+{
 	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t i;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
 	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 
-	for (Py_ssize_t i = 0; i < size; i++) {
+	for (i = 0; i < size; i++)
+	{
 		PyObject *item = PyList_GetItem(p, i);
 		const char *type = Py_TYPE(item)->tp_name;
 
 		printf("Element %zd: %s\n", i, type);
 	}
+}
+
+/**
+ * print_python_bytes - Prints basic info about Python bytes objects
+ * @p: PyObject pointer
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size, i;
+
+	size = PyBytes_Size(p);
+
+
+	printf("[.] bytes object info\n");
+	printf(" size: %zd\n", size);
+	printf(" trying string: %s\n", PyBytes_AsString(p));
+	printf(" first %zd bytes:", size + 1 > 10 ? 10 : size + 1);
+	for (i = 0; i < (size + 1 > 10 ? 10 : size + 1); i++)
+		printf(" %02x", (unsigned char)PyBytes_AsString(p)[i]);
+	printf("\n");
 }
