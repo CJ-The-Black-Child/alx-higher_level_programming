7c101a8510e45b578ba99c3c3721e3d2efabe2e8
Congo Justus Migue <congomigue@gmail.com>
2023-05-17 21:07:07 +0000

    #11 fix error test1

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,4 +2,7 @@
 
 
 def multiple_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+    return list(map(lambda x: multiply(x, number), my_list))
+
+def multiply(x, number):
+    return x * number
