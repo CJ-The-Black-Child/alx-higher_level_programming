d8da594bb739fdfd15c681d543330169afdbef15
Congo Justus Migue <congomigue@gmail.com>
2023-05-29 23:32:01 +0000

    Fixed PEP8 styling issues

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,8 +9,8 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 printed += 1
-         except IndexError:
-            pass
+        except IndexError:
+            break
 
     print()
     return printed
