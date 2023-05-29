adade50dd300d12835355784748427dbf33fdd80
Congo Justus Migue <congomigue@gmail.com>
2023-05-29 20:43:04 +0000

    More Ammends to 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,12 @@
 def safe_print_list(my_list=[], x=0):
     printed = 0
 
-    for i in range(min(x, len(my_list))):
-        print(my_list[i], end='')
-        printed += 1
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            printed += 1
+    except IndexError:
+        pass
 
     print()
     return printed
