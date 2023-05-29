2d625cc5544e264501f7dd2af39ceee13ab2793a
Congo Justus Migue <congomigue@gmail.com>
2023-05-29 23:29:54 +0000

    Check code #2 fixing try errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,14 +4,13 @@
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
 
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             if isinstance(my_list[i], int):
-                continue
                 print("{:d}".format(my_list[i]), end='')
                 printed += 1
-    except IndexError:
-        pass
+         except IndexError:
+            pass
 
     print()
     return printed
