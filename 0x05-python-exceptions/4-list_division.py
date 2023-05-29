a0951b93aa6dc9b0152f16391ca42704f362ca33
Congo Justus Migue <congomigue@gmail.com>
2023-05-29 22:43:09 +0000

    check #2 task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
             if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError("out of range")
