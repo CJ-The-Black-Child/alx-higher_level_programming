7ef2ac4051b6ccedba03972d014d5ae60746e3e1
Congo Justus Migue <congomigue@gmail.com>
2023-05-29 23:07:48 +0000

    Last check #4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(0, list_length):
@@ -13,13 +11,13 @@ def list_division(my_list_1, my_list_2, list_length):
 
         except TypeError:
             print("wrong type")
-            result.append(0)
+            division_result = 0
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
+            division_result = 0
         except IndexError:
             print("out of range")
-            result.append(0)
+            division_result = 0
         finally:
             result.append(0)
     return result
