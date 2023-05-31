3a45c721c723e2a1158cfde563487c0546401cc8
Congo Justus Migue <congomigue@gmail.com>
2023-05-31 00:04:38 +0000

    bug fixes on 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,12 +4,14 @@
 
 
 class Square:
-
+    """Represents a square"""
     def __init__(self, size=0):
         """
         Initializes a Square instance
+
         Args:
         size (init): The size of the square (default is 0)
+
         Raises:
         TypeError: if size is not an integer
         ValueError: if size is less than 0
