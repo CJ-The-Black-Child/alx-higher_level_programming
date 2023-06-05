a8ddea12341fe3c28a9afdf4262423e071462c5b
Congo Justus Migue <congomigue@gmail.com>
2023-06-05 18:18:09 +0000

    #10. N queens

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self._width =width
+        self._height = height
 
     @property
     def width(self):
-        return self.__width
+        return self._width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        self._width = value
 
     @property
-    def height(Self):
-        return self.__height
+    def height(self):
+    return self._height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        self._height = value
--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,63 @@
 #!/usr/bin/python3
+import sys
+
+    def is_self(board, row, col):
+        # Check if there is a queen in the same column
+        for i in range(row):
+            if board[i] == col:
+                return false
+        # Check if there is a queen in the upper left diagonal
+        i = row - 1
+        j = col - 1
+        while i >= 0 and j >= 0:
+            if board[i] == j:
+                return False
+            i -= 1
+            j -= 1
+        # check if ther is a queen in the upper right diagonal
+        i = row - 1
+        j = col + 1
+        while i >= 0 and j < len(board):
+            if board[i] == j:
+                return False
+            i -= 1
+            j += 1
+        return True
+
+    def solve_n_queens(n):
+        board = [-1] * n 
+    
+    def place_queen(row):
+        if row == n:
+            print ([[i, board[i]] for i in range(n)])
+            return
+
+            for col in range(n):
+                if is_safe(board, row, col):
+                    board[row] = col
+                    place_queen(row + 1)
+                    board[rw] = -1
+        place_queen(0)
+
+# Main Program
+if __name__ == '__main__':
+    # check if the correct number of arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    # Get the value of N from command line argument
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    # Check if N is at least 4
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    # Solve the N-Queens Problem
+    solve_n_queens(n)
+
