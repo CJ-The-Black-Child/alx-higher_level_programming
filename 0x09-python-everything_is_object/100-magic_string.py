7893864ec56903398a5c3d687d8187e4599237df
Congo Justus Migue <congomigue@gmail.com>
2023-06-06 14:03:12 +0000

    updated code to meet the pep8 code style

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def magic_string():
+    """
+    Returns a string "BestSchool" n times the number of the iteration.
+    """
+    magic_string.counter = getattr(magic_string, "counter", 0) + 1
+    return "BestSchool, " * (magic_string.counter - 1) + "BestSchool"
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+class LockedClass:
+    """
+    Class that prevents the user from dynamically creating new instance
+    attributes, except if the new instance attribute is called first_name.
+    """
+    __slots__ = ["first_name"]
