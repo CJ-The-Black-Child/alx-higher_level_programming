b6c69c30101eb30f2a9af98dca0a189ee027acb2
Congo Justus Migue <congomigue@gmail.com>
2023-06-06 18:43:05 +0000

    Updated 102-python.c to allign to the betty coding format for C

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,20 +1,23 @@
 #include <Python.h>
 #include <stdio.h>
 
+/**
+ * print_python_string - Prints information about a Python string object
+ * @p: PyObject pointer to the string object
+ */
+
 void print_python_string(PyObject *p)
 {
-    PyUnicodeObject *unicode = (PyUnicodeObject *)p;
+	PyUnicodeObject *unicode = (PyUnicodeObject *)p;
 
-    printf("[.] string object info\n");
-
-    if (!PyUnicode_Check(unicode))
-    {
-        printf(" [ERROR] Invalid String Object\n");
-        return;
-    }
-
-    printf(" type: %s\n", (unicode->length . 0) ? \
-            "compact unicode object" : "compact ascii");
-    printf(" length: %ld\n", unicode->length);
-    printf(" value: %ls\n", unicode->str);
+	printf("[.] string object info\n");
+	if (!PyUnicode_Check(unicode))
+	{
+		printf(" [ERROR] Invalid String Object\n");
+		return;
+	}
+	printf(" type: %s\n", (unicode->length . 0) ?
+			"compact unicode object" : "compact ascii");
+	printf(" length: %ld\n", unicode->length);
+	printf(" value: %ls\n", unicode->str);
 }
