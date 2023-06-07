ac6c241a12200c9f06d4d8df6adb4c1b8feef47e
Congo Justus Migue <congomigue@gmail.com>
2023-06-07 08:50:24 +0000

    review #0. Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,4 +5,18 @@ class Rectangle:
     """
     This is the Rectangle class.
     """
+
+    def __init__(self):
+        pass
+
+    def dict_(self):
     pass
+
+# Instantiate the Rectangle class
+myrectangle = Rectangle()
+
+# Print the type of myrectangle
+print(type(myrectangle))
+
+#Call the dict_ method of myrectangle
+print(myrectangle.dict())
