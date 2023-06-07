a19fd87f04d3c2c18fd4c0397b9f8e1f247ed61e
Congo Justus Migue <congomigue@gmail.com>
2023-06-07 09:46:55 +0000

    qa review #1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
+
+
 class Rectangle:
     def __init__(self, width=0, height=0):
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self._width =width
-        self._height = height
+        self.__width = 0
+        self.__height = 0
+        self._Rectangle__width = width
+        self._Rectangle__height = height
 
     @property
     def width(self):
-        return self._width
+        return self._Rectangle__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        self._width = value
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._Rectangle__width = value
 
     @property
     def height(self):
-    return self._height
+        return self._Rectangle__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        self._height = value
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._Rectangle__height = value
