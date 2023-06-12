68b842f3281385bdf5193b1cd8ba2481a6338a15
Congo Justus Migue <congomigue@gmail.com>
2023-06-12 17:51:59 +0000

    task #4: Only sub class of... and #5: Geometry module

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """Checks if an object inherited (directly or indirectly) from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object inherited (directly or indirectly)
+    from the specified class;
+        False otherwise.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
