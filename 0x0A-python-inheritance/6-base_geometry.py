10f0ae24c937a0938a137dffb4e1e8171d9ef36c
Congo Justus Migue <congomigue@gmail.com>
2023-06-12 17:52:43 +0000

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Class representing base geomtry."""
+
+    def area(self):
+        """Raises an exception indicating that area() is not implemented."""
+        raise Exception("area() is not implemented")
