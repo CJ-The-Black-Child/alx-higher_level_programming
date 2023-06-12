514e96274a8c0da74a4b892c2c1e249ff6a47a2d
Congo Justus Migue <congomigue@gmail.com>
2023-06-12 19:06:02 +0000

    11. Square #2

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class MyInt(int):
+    """Class representing a rebel integer."""
+
+
+    def __eq__(self, other):
+        """Checks if the integer is not equal to the specified value.
+
+            Args:
+                other: The value to compare against.
+
+            Returns:
+                True if the integer is not equal to the specified value;
+                False otherwise.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Checks if the integer is equal to the specified value.
+
+            Args:
+                other: The value to compare against.
+
+            Returns:
+                True if the intger is equal to the specified value;
+                False otherwise.
+        """
+        return super().__eq__(other)
