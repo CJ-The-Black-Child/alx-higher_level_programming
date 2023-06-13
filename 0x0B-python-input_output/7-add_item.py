894b5c37a264de73b2a6368ce2a86078617808f5
Congo Justus Migue <congomigue@gmail.com>
2023-06-13 22:14:17 +0000

    modified task #7: Load, Add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,20 @@ import sys
 from load_from_json_file import load_from_json_file
 from save_to_json_file import save_to_json_file
 
-# Load existing items from file
+"""
+Load existing items from file
+"""
 try:
     items = load_from_json_file('add_item.json')
 except FileNotFoundError:
     items = []
 
-# Add new items from command line Arguments
+"""
+    Add new items from command line Arguments
+"""
 items.extend(sys.argv[1:])
 
-# Save Updated items to file
+"""
+Save Updated items to file
+"""
 save_to_json_file(items, 'add_item.json')
