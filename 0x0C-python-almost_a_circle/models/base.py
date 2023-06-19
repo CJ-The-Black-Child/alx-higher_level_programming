1b417f6f28cab647c98055b80759b52b0946c1e2
Congo Justus Migue <congomigue@gmail.com>
2023-06-19 19:47:42 +0000

    15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,3 +1,5 @@
+import json
+
 class Base:
     """ Private class attribute to track the number of objects"""
     __nb_objects = 0
@@ -17,3 +19,8 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
