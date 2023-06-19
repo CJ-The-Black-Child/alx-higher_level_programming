ea804a71d369267722a4697ab16d3e50eee12953
Congo Justus Migue <congomigue@gmail.com>
2023-06-19 20:07:59 +0000

    #17. JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,19 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        obj_list = []
+        if list_objs is not None:
+            obj_list = [obj.to_dictionary() for obj in list_objs]
+        json_str = cls.to_json_string(obj_list)
+        with open(filename, "w") as file:
+            file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
