5573cde88b4954cdf2a518c9a835c482e51b9e19
Congo Justus Migue <congomigue@gmail.com>
2023-06-19 19:40:41 +0000

    #14. Square instance to dictionary representation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y,
+        }
+
     @property
     def size(self):
         return self.width
@@ -14,7 +22,7 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def uodate(self, *args, **kwargs):
+    def update(self, *args, **kwargs):
         if args:
             attrs = ["id", "size", "x", "y"]
             for i, arg in enumerate(args):
