a4980b1c75e9a3adb9719114b0092b97eba31b3c
Congo Justus Migue <congomigue@gmail.com>
2023-07-19 20:48:44 +0000

    fixes on task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY `user_0d_2_pwd`;
 
 -- Grant select privilege to 'user_0d_2` on the database hbtn_0d_2
-GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
+GRANT SELECT ON `hbtn_0d_2`.`*` TO `user_0d_2`@`localhost`;
 
 -- Flush the privileges to apply the changes
 FLUSH PRIVILEGES;
