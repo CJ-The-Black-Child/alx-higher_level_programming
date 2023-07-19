35822e5866cae496647fb0f0bfe8d82e917d86e6
Congo Justus Migue <congomigue@gmail.com>
2023-07-19 20:41:56 +0000

    #quiz 18 more debugging

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,14 +1,16 @@
 -- Use the database `hbtn_0d_tvshows`
 USE `hbtn_0d_tvshows`;
 
--- Get the genre_id for the genre 'Comedy'
-SELECT `id` FROM `tv_genres` WHERE `name` = 'Comedy';
-
 -- List all shows without the genre 'Comedy'
-SELECT `title` FROM `tv_shows`
+SELECT `title`
+FROM `tv_shows`
 WHERE `id` NOT IN (
-	SELECT `tv_show_id` FROM `tv_show_genres` WHERE `genre_id` = (
-		SELECT `id` FROM `tv_genres` WHERE `name` = 'Comedy'
-	       )
-)
+	SELECT `tv_show_id`
+	FROM `tv_show_genres`
+	WHERE `genre_id` = (
+		SELECT `id`
+		FROM `tv_genres`
+		WHERE `name` = 'Comedy'
+		)
+	)
 ORDER BY `title` ASC;
