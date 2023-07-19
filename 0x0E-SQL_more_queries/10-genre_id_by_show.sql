282bc91dc8feb7ce4242c661ab03b2725faf7e95
Congo Justus Migue <congomigue@gmail.com>
2023-07-19 19:08:12 +0000

    #10. Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Use the database `hbtn_0d_tvshows`
+USE `hbtn_0d_tvshows`;
+
+-- List all shows with their corresponding genre_ids that
+-- at least one genre linked
+SELECT `tv_shows`.`title`, `tv_shows_genres`.`genre_id`
+FROM `tv_shows`
+JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`tv_show_id`
+ORDER BY `tv_shows`.`title` ASC, `tv_show_genres`.`genre_id` ASC;
