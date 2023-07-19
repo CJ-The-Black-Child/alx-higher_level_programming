79c2482296b8d84ed2d17e4a3ca4af5930724fd8
Congo Justus Migue <congomigue@gmail.com>
2023-07-19 19:36:15 +0000

    #15. Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- use the database `hbtn_0d_tv_shows`
+USE `hbtn_0d_tvshows`;
+
+-- List all Comedy shows from the database
+SELECT `tv_shows`.`title`
+FROM `tv_shows`
+JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`tv_show_id`
+JOIN `tv_genres` ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
+WHERE `tv_genres`.`name` = `Comedy`
+ORDER BY `tv_shows`.`title` ASC
