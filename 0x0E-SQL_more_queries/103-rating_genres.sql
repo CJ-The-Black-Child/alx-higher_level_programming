69d2c3cec959f017b3e723e6ef28b9676b540ba1
Congo Justus Migue <congomigue@gmail.com>
2023-07-19 20:05:04 +0000

    #20. Best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- Use the database hbtn_0d_tvshows_rate
+USE hbtn_od_tvshows_rate;
+
+-- List all genres by their rating sum
+SELECT tv_genres.name, SUM(rating) AS rating_sum
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv.show_genres.genre_id
+JOIN tv_show_ratings ON tv_show_genres.tv_show_id = tv_show_ratings.tv_show_id
+GROUP BY tv_genres.name
+ORDER BY rating_SUM DESC;
