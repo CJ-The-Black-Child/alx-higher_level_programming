8adc6437527c44c24e7c702357cc3aef48607842
Congo Justus Migue <congomigue@gmail.com>
2023-07-19 18:41:49 +0000

    #7. Cities table

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,8 @@
 -- Create the table unique_id if i doesn't exist
 CREATE TABLE IF NOT EXISTS unique_id (
-		id INT DEFAULT ! UNIQUE,
+		id INT DEFAULT 1 UNIQUE,
 		name VARCHAR(256)
 		);
+
 -- Flush the privileges to apply the changes
 FLUSH PRIVILEGES;
