0baeea55db8e54704f266cfb20df85eab1dd0ab6
Congo Justus Migue <congomigue@gmail.com>
2023-07-19 18:14:43 +0000

    #1. Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create the user `user_0d_1` if it doesn't exist
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
+
+-- Grant all privileges to `user_0d_1` on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+
+-- Flush the privileges to apply changes
+FLUSH PRIVILEGES;
