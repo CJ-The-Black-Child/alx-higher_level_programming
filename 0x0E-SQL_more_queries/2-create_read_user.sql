2afe3b683877fbb665c749ec16cf58f9d387ae8f
Congo Justus Migue <congomigue@gmail.com>
2023-07-19 18:24:07 +0000

    #2. Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Ceate the database hbtn_0d_2 if it doesn't exist
+CREATE DATABASE IF NOT EXISTTS hbtn_0d_2;
+
+-- Create the user `user_0d_2` if it doesn't exist
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BU `user_0d_2_pwd`;
+
+-- Grant select privilege to 'user_0d_2` on the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO `user_0d_2`@`localhost`;
+
+-- Flush the privileges to apply the changes
+FLUSH PRIVILEGES;
