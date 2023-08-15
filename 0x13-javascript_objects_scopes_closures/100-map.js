150a8b8363a416bcf0a577e1b88db845dfb8416d
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-15 16:12:04 +0000

    Added advanced task #11. Factor index

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const list = require('./100-data').list;
+
+// Use the map function to create a new list with each value multiplied by its index
+const newList = list.map((value, index) => value * index);
+
+console.log(list);
+console.log(newList);
+
