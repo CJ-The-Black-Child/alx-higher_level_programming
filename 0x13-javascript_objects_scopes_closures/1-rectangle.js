8c9ea19269898c759be97f29382cf22fba56cfd4
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-15 15:17:17 +0000

    Added task #2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-// Define the Rectangle class using the class notation
 module.exports = class Rectangle {
 	constructor(w, h) {
-			//Initialize instance attributes width and height
 			this.width = w;
 			this.height = h;
 		}
