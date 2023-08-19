79e3272159972d4dcd05e122cad7911b64ac80e0
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-19 16:48:26 +0000

    Added task 2 fixes for file 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,18 +10,17 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-    """ Check for correct number of arguments """
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(
-            sys.argv[0]))
-        sys.exit(1)
-
     """ Get MySQL credentials and state name from command line arguments """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
+    """ Connect to the MySQL server"""
+    db = MySQLdb.connect(
+            host="localhost", port=3306,
+            user=username,passwd=password,db=database
+            )
     """ Create a cursor to interact with the db """
     cursor = db.cursor()
 
