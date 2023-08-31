ec2edf2ba9bad07f9d102d56408ef60d631ba714
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-31 22:09:49 +0000

    Fixed bug on task 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,20 +4,22 @@
 
 def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers"""
+    size = len(list_of_integers)
     if list_of_integers == []:
         return None
 
-    size = len(list_of_integers)
     if size == 1:
         return list_of_integers[0]
     elif size == 2:
         return max(list_of_integers)
 
-    mid = int(size / 2)
+    mid = int(size // 2)
     peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+    left = list_of_integers[mid - 1]
+    right = list_of_integers[mid + 1]
+    if peak > left and peak >= right:
         return peak
-    elif peak < list_of_integers[mid - 1]:
+    elif peak < left:
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
