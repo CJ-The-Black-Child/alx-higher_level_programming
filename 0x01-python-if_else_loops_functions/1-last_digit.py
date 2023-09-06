647b0d2c74cb286005e1cc878d11449dc7e6180f
CJ-The-Black-Child <congomigue@gmail.com>
2023-09-06 15:32:08 +0000

    Fixed pycodestyle related bugs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,16 @@ if number < 0:
     last_digit *= -1
 
 if (last_digit > 5):
-    print("Last digit of {} is {} and is greater than {}".format(number, last_digit, 5))
+    print(
+            "Last digit of {} is {} and is greater than {}".format(
+                number, last_digit, 5
+                )
+            )
 elif (last_digit == 0):
     print("Last digit of {} is {} and is {}".format(number, last_digit, 0))
 else:
-    print("Last digit of {} is {} and is less than {} and not {}".format(number, last_digit, 6, 0))
+    print(
+            "Last digit of {} is {} and is less than {} and not {}".format(
+                number, last_digit, 6, 0
+                )
+            )
