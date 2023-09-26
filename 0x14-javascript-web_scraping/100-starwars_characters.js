fddc92a5a2ff75260591b28bafb0e8d1d5dfc5ab
CJ-The-Black-Child <congomigue@gmail.com>
2023-09-26 22:11:21 +0000

    script that prints all characters of a Star Wars movie:

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// This script prints all characters of a Star wars movie
+
+const request = require('require');
+const film = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${film}`;
+request(url, (err, res, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (err, res, body) {
+        if (!err) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
